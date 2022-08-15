626784390bdbaa3790fd065390c6b0204d0a118b
KanaKarimata <kana.lt22@gmail.com>
2022-08-15 18:48:23 +0900

    finish Part8-93

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, Your are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      output = `NEW OUTPUT!`;
+    }
+    // console.log(str);
+    console.log(millenial);
+    console.log(output);
+    // add(2,3);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
