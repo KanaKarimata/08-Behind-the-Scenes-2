014b5e269b41f6acfeb24c1509ff134702b1811d
KanaKarimata <kana.lt22@gmail.com>
2022-08-20 21:07:20 +0900

    finish part8-98

--- script.js
+++ script.js
@@ -74,33 +74,79 @@
 
 // move to another repository
 
-console.log(this);
-const calcAge = function(birthYear) {
-  console.log(2037 - birthYear);
-  // console.log(this);
-}
-calcAge(1991);
+// console.log(this);
+// const calcAge = function(birthYear) {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// }
+// calcAge(1991);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  // console.log(this);
-};
-calcAgeArrow(1980);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAgeArrow(1980);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function() {
+//     // console.log(this);
+//     console.log(2037 -this.year);
+//   }
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017
+// };
+
+// matilda.calcAge = jonas.calcAge
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+
+// var firstName = 'Matilda';
 
 const jonas = {
+  firstName: 'Jonas',
   year: 1991,
   calcAge: function() {
     // console.log(this);
     console.log(2037 -this.year);
-  }
+    
+    //solution1
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // }
+    
+    //solution2
+    const isMillenial = () => {
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(`Hey ${this.firstName}`)
+  },
 };
+
+jonas.greet();
+console.log(this.firstName);
 jonas.calcAge();
 
-const matilda = {
-  year: 2017
+//Argument keyword
+const addExpr = function (a, b) {
+  console.log(arguments)
+  return a + b;
+}
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
 };
 
-matilda.calcAge = jonas.calcAge
-matilda.calcAge();
-
-const f = jonas.calcAge;
+addArrow(2,5,8);
